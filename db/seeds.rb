b415fcbd83e125bedb079c8dc1c14e114e1403b7
Nikhil Pathak <nikhil.pathak@velotio.com>
2020-01-15 17:13:52 +0530

    Updated seed file for user dummy data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+first_names = [ 'Angelica', 'Cedric', 'Paul', 'Alexander', 'Serge' ]
+last_names = [ 'Ramos', 'Kelly', 'Bruce', 'Mccray', 'Baldwin' ]
+
+3000.times do
+  first_name = Faker::Name.first_name
+  last_name = Faker::Name.last_name
+  name = [ first_name, last_name ].join(' ')
+  email = first_name + last_name + '@sampleblog.com'
+  User.create(name: name, email: email, mobile_number: rand(9 ** 9))
+end
