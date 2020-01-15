0b5c5cd8fae6840f4fa601fb37668770c92d8876
Nikhil Pathak <nikhil.pathak@velotio.com>
2020-01-15 15:50:58 +0530

    Added blogpost model + create migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_01_15_101059) do
+ActiveRecord::Schema.define(version: 2020_01_15_101812) do
+
+  create_table "blogposts", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "title"
+    t.string "body"
+    t.datetime "published_date"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name"
