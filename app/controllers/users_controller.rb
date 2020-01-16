125e0f8d584a67e95767afa7926f87c438c0ce01
Nikhil Pathak <nikhil.pathak@velotio.com>
2020-01-16 12:28:41 +0530

    User update functionality

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,4 +2,17 @@ class UsersController < ApplicationController
   def index
     @users = User.all
   end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    user_attrs = params[:user]
+    @user.update(name: user_attrs[:name],
+      email: user_attrs[:email],
+      mobile_number: user_attrs[:mobile_number])
+    redirect_to users_path
+  end
 end
